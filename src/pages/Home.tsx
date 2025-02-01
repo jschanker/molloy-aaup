5910498a1d67095c13de056d49089a9d9ac71353
jschanker <jschanker@molloy.edu>
2025-02-01 18:40:37 -0500

    fix: frameborder => frameBorder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -100,7 +100,7 @@ export default function Home() {
             height="315"
             src="https://www.youtube.com/embed/xSPG-oM4cks?si=gihPuQPW75TChjPk"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
