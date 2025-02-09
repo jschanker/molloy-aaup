1f7fba5637b14a61fd9acc8ec209f2b8d52f2913
jschanker <jschanker@molloy.edu>
2025-02-08 19:44:36 -0500

    Add Codifying the Redbook meeting slides

--- src/pages/CodifyingRedBook.tsx
+++ src/pages/CodifyingRedBook.tsx
@@ -1,18 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function CodifyingRedBook() {
   return (
     <div className="container-fluid">
-      <h1>Register for Codifying the Red Book Workshop on Zoom</h1>
+      <h1>Meeting notes from Codifying the Red Book Virtual Workshop</h1>
       <p>
-        Sign up for "Codifying the Redbook", a virtual workshop about ways to
-        gain or strengthen policies in your union contract of faculty handbook
-        to protect academic freedom, faculty oversight of curriculum, protest
-        and dissent, due process and more:{' '}
-        <a
-          href="https://us06web.zoom.us/meeting/register/uylHmzvGSqqQfe1Igd0-Aw#/registration"
-          target="_blank"
-        >
-          https://us06web.zoom.us/meeting/register/uylHmzvGSqqQfe1Igd0-Aw#/registration
-        </a>
+        Codifying the Redbook is a virtual workshop about ways to gain or
+        strengthen policies in your union contract of faculty handbook to
+        protect academic freedom, faculty oversight of curriculum, protest and
+        dissent, due process and more. The Spring 2025 workshop was held on
+        February 6, 2025. <br />
+        <Link to="/assets/Codifying_the_Redbook_Spring_2025.pdf">
+          Download the Slides from the Meeting
+        </Link>
       </p>
     </div>
   );
